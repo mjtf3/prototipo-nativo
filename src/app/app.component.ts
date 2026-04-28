ae083e6066c0fd1d65903a3b3a5450f3329e0ae6
Manuel Tornero <mjtf3@alu.ua.es>
2026-04-28 18:07:35 +0200

    muy facil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Lista } from "./models/lista";
 
 @Component({
   selector: "app-root",
@@ -8,6 +9,45 @@ import { Component, OnInit } from "@angular/core";
 export class AppComponent implements OnInit {
   title = "appListas";
   msg = "Hola, bienvenido a mi proyecto en Angular";
+  mostrarFormulario = false;
+  nueva: Lista = new Lista();
+  listas: Lista[] = [
+    {
+      nombre: "Compra",
+      descripcion: "Productos pendientes para comprar",
+      color: "bg-primary text-white",
+      visible: true,
+    },
+    {
+      nombre: "Trabajo",
+      descripcion: "Tareas pendientes del proyecto",
+      color: "bg-success text-white",
+      visible: true,
+    },
+    {
+      nombre: "Estudios",
+      descripcion: "Temas para repasar esta semana",
+      color: "bg-warning text-dark",
+      visible: true,
+    },
+  ];
+
+  abrirFormulario(): void {
+    this.mostrarFormulario = true;
+    this.nueva = new Lista();
+  }
+
+  guardarLista(): void {
+    this.listas.push({ ...this.nueva });
+    this.nueva = new Lista();
+    this.mostrarFormulario = false;
+  }
+
+  cancelarFormulario(): void {
+    this.nueva = new Lista();
+    this.mostrarFormulario = false;
+  }
+
   ngOnInit(): void {
     setTimeout(() => {
       this.msg = "";
